aaf97b4ea6ddb262c072a10c3744d4487295a9a3
Zerhouni Omar <hddjdndjdndj@gmail.com>
2025-01-15 09:56:44 +0100

    Modified: jsx to js, add Rooter configue

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Courses from "./pages/Courses/Courses";
+import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Auth/Login";
+
+const ConfigueRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default ConfigueRouter;
